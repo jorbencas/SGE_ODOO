76cda866f7ebe25777b2cfeb0ee90b0a2e51c34f
jorbencas <jorbencas@gmail.com>
2018-10-31 11:03:32 +0000

    Starting to create dummies files

--- reserva_hoteles/models/models.py
+++ reserva_hoteles/models/models.py
@@ -20,28 +20,29 @@ class citys(models.Model):
 	description = fields.Text()
 	location = fields.Text()
 	countrys = fields.Many2one('res.country','Pais')
-	listHotels = fields.Many2one('reserva_hoteles.hotels')
+	listHotels = fields.One2many('reserva_hoteles.hotels', 'name')
 
 class hotels (models.Model):
     _name = 'reserva_hoteles.hotels'
     name = fields.Text()
     photoGallery = fields.One2many('reserva_hoteles.hotelgallery','photo')
     description = fields.Text()
-    listRooms = fields.Many2many('reserva_hoteles.rooms')
+    listRooms = fields.One2many('reserva_hoteles.rooms')
     valorations = fields.Selection([('1', '⭐'), ('2', '⭐⭐'), ('3', '⭐⭐⭐'), ('4', '⭐⭐⭐⭐'), ('5', '⭐⭐⭐⭐⭐')])
     listServices = fields.Many2many('reserva_hoteles.service')
     city = fields.Many2one('reserva_hoteles.citys','name')
-    comments = fields.Many2many('reserva_hoteles.comments')
+    comments = fields.One2many('reserva_hoteles.comments')
 
 class rooms (models.Model):
     _name = 'reserva_hoteles.rooms'
     name = fields.Integer()
     beds = fields.Selection([('1','Una Cama'),('2','Dos Camas'),('3','Cama de Matrimonio'),('4','Cama de matromonio mas cama infantil') ],'Type', default='1')
-    photos = fields.Many2many('reserva_hoteles.photogallery')
+    photos = fields.One2many('reserva_hoteles.photogallery','photo')
     price = fields.Float(default=1)
     description = fields.Text(default="Habitación grande, espaciosa y con gran luminosidad.")
     hotel = fields.Many2one('reserva_hoteles.hotels','name')
-    
+    city = fields.Many2one('reserva_hoteles.citys', related='hotel.city', readonly=True)
+
 class reserve (models.Model):
     _name = 'reserva_hoteles.reserve'    
     name = fields.Char()
@@ -50,13 +51,13 @@ class reserve (models.Model):
     client = fields.Many2many('res.partner')
     room = fields.Many2one('reserva_hoteles.rooms')
     hotel = fields.Many2one('reserva_hoteles.hotels', related='room.hotel', readonly=True)
-    city = fields.Many2one('reserva_hoteles.citys', related='hotel.listRooms', readonly=True)
+    city = fields.Many2one('reserva_hoteles.citys', related='hotel.city', readonly=True)
 
 class photoHotel (models.Model):
     _name='reserva_hoteles.hotelgallery'
     name = fields.Text()
     photo = fields.Binary()
-    hotel = fields.Many2one('reserva_hoteles.hotels')
+    hotel = fields.Many2one('reserva_hoteles.hotels'related='')
 
 class photoGallery (models.Model):
     _name = 'reserva_hoteles.photogallery'
@@ -67,7 +68,7 @@ class services (models.Model):
     _name = 'reserva_hoteles.service'
     name = fields.Selection([("1","Parking"),("2","Roomservice"),("3","Sauna")],'Type', default='2')
     photo = fields.Binary()
-    hotel = fields.One2many('reserva_hoteles.hotels','name')
+    hotel = fields.Many2many('reserva_hoteles.hotels','name')
 
 class comments (models.Model):
     _name = 'reserva_hoteles.comments'
